301fa5a96db537246406255d5f3770e4f1ead4e3
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-11-01 21:09:55 -0500

    Add cobra for CLI args and viper for configuration

---
+++ cmd/root.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// > app
+var rootCmd = &cobra.Command{
+	Use:   "godoro",
+	Short: "A simple pomodoro CLI written in Go",
+	Long:  `Manage your life or something...`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome!")
+	},
+}
+
+// Initialize child commands and flags
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().
+		StringVar(&cfgFile, "config", "", "config file (default is $HOME/.godoro.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".godoro" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".godoro")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/fatih/color"
 
+	"github.com/cronJohn/godoro/cmd"
 	"github.com/cronJohn/godoro/utils/logger"
 )
 
 var flags int = log.Ldate | log.Ltime | log.Lshortfile
 
-func main() {
-	fmt.Println("Welcome!")
-
-	lg := logger.LoggerAggregate{
+func init() {
+	Lg := logger.LoggerAggregate{
 		InfoLogger:    log.New(os.Stdout, color.CyanString("[INFO] "), flags),
 		WarningLogger: log.New(os.Stdout, color.YellowString("[WARNING] "), flags),
 		ErrorLogger:   log.New(os.Stderr, color.RedString("[ERROR] "), flags),
 	}
 
-	lg.Info("This is an info message")
-	lg.Warning("This is a warning message")
-	lg.Error("This is an error message")
+	Lg.Info("Logger created...")
+}
+
+func main() {
+	cmd.Execute()
 }
