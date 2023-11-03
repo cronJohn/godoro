83be2763ef6fe500757ffed7f5ac56742a2e893b
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-11-02 19:06:24 -0500

    Add zerolog as a dedicated logger
    - Fix issue with 'log.Lshortfile' showing 'logger.go' as the source file despite caller
    
    - This logger is not only fast but also provides structured and colored output

--- main.go
+++ main.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	"log"
 	"os"
 
-	"github.com/fatih/color"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	"github.com/cronJohn/godoro/cmd"
-	"github.com/cronJohn/godoro/utils/logger"
 )
 
-var flags int = log.Ldate | log.Ltime | log.Lshortfile
-
-func init() {
-	Lg := logger.LoggerAggregate{
-		InfoLogger:    log.New(os.Stdout, color.CyanString("[INFO] "), flags),
-		WarningLogger: log.New(os.Stdout, color.YellowString("[WARNING] "), flags),
-		ErrorLogger:   log.New(os.Stderr, color.RedString("[ERROR] "), flags),
-	}
-
-	Lg.Info("Logger created...")
-}
-
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Logger = log.With().Caller().Logger() // add file name and line number
+
 	cmd.Execute()
 }
